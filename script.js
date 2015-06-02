1a2d34f1ea7c5d0aead93e661e5d7557d3f7d367
katkor <k.korzeniecka@gmail.com>
2015-06-02 20:14:20 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var allQuestions = [
+{question: "W którym wieku panowała caryca Katarzyna II Wielka?",
+choices: ["XVI", "XVII", "XVIII"],
+correctAnswer:2},
+{question: "Ile dzielników ma liczba pierwsza?",
+choices: ["1", "2", "nieskończoność"],
+correctAnswer:1},
+{question: "Ile kwintali liczy jedna decytona?",
+choices: ["100", "10", "1"],
+correctAnswer:2},
+{question: "Co robią koty?",
+choices: ["miauczą", "skakają", "czczą Szatana"],
+correctAnswer:1},
+{question: "Czym można otworzyć piwo?",
+choices: ["wszystkim", "otwieraczem", "zębami"],
+correctAnswer:0},
+{question: "Więcej niż jedno zwierzę to:",
+choices: ["owca", "lama", "stado"],
+correctAnswer:0}
+];
